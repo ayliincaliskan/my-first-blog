81c80939a0eaf131f8f24775df991a6e731a2055
aylin <aylin@example.com>
2017-03-25 16:42:40 +0300

    basliklari geri cagirdim

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,6 @@ from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-	posts = Post.objects.filter(yayinlanma_tarihi__lte=timezone.now()).order_by('yayinlanma_tarihi')
+	posts = Post.objects.order_by('yayinlanma_tarihi')
 	return render(request, 'blog/post_list.html', {'posts': posts})
+
